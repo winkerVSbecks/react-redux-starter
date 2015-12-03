b3d3a45fcd4ac95ac149bc4139cbf87409f1dc1a
andrewdamelio <dev@andrewdamel.io>
2015-12-03 13:22:02 -0500

    update LoginModal comments

--- src/components/login/LoginModal.js
+++ src/components/login/LoginModal.js
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React, { PropTypes } from 'react';
 import Modal from '../ui/Modal';
 import ModalContent from '../ui/ModalContent';
 import LoginForm from '../login/LoginForm';
 
+/**
+ * A React component that displays a modal use to show the login form.
+ *
+ * Example usage:
+ *
+ * ```
+ * renderExample() {
+     render(
+       <LoginModal
+         onSubmit={ login }
+         isPending={ session.get('isLoading', false) }
+         hasError={ session.get('hasError', false) }
+         isVisible={ !isLoggedIn } />
+     );
+ * }
+ * ```
+ */
 const LoginModal = ({ isVisible, isPending, hasError, onSubmit }) => {
   return (
     <Modal isVisible={ isVisible }>
@@ -18,6 +34,27 @@ const LoginModal = ({ isVisible, isPending, hasError, onSubmit }) => {
     </Modal>
   );
 };
+
+LoginModal.displayName = 'LoginModal';
+LoginModal.propTypes = {
+  /**
+   * If true, show this component
+   */
+  isVisible: PropTypes.bool.isRequired,
+  /**
+   * If true, show loading indicator
+   */
+  isPending: PropTypes.bool.isRequired,
+  /**
+   * If true, display error state
+   */
+  hasError: PropTypes.bool.isRequired,
+  /**
+   * function to handle the login form submit
+   */
+  onSubmit: PropTypes.func.isRequired,
+};
+
 
 const styles = {
   base: {
